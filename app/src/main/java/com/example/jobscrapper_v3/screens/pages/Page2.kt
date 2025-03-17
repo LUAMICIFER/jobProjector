6c5c85a62cddf842b99bc04c362d01907c09f410
ANKIT RAJ <96227537+LUAMICIFER@users.noreply.github.com>
2025-03-17 19:50:07 +0530

    Delete app/src/main/java/com/example/jobscrapper_v3/screens/pages/Page2.kt

--- app/src/main/java/com/example/jobscrapper_v3/screens/pages/Page2.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jobscrapper_v3.screens.pages
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-
-@Composable
-fun Page2(navController: NavController) {
-
-}
